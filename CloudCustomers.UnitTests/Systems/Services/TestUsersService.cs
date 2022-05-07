172f3f7aac3ae83220a0dd85a77c7b136102e323
Roslava Angelova <88967550+roangelova@users.noreply.github.com>
2022-05-07 11:42:56 +0300

    created TestUsersService

---
+++ CloudCustomers.UnitTests/Systems/Services/TestUsersService.cs
@@ -0,0 +1,43 @@
+﻿using CloudCustomers.Api.Models;
+using CloudCustomers.Api.Services;
+using CloudCustomers.UnitTests.Fixtures;
+using CloudCustomers.UnitTests.Helpers;
+using Moq;
+using Moq.Protected;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CloudCustomers.UnitTests.Systems.Services
+{
+    public class TestUsersService
+    {
+        [Fact]
+        public async Task GetAllUsers_OnInvoke_InvokesHttpGetRequest()
+        {
+            var expectedResponse = UsersFixture.GetTestUsers();
+            var handlerMock = MockHttpMessageHandler<User>.SetupBasicGetResorceList(expectedResponse);
+            var httpClient = new HttpClient(handlerMock.Object);
+            var sut = new UsersService(httpClient);
+
+            await sut.GetAllUsers();
+
+            handlerMock
+                .Protected()
+                .Verify(
+                "SendAsync",
+                Times.Exactly(1),
+                ItExpr.Is<HttpRequestMessage>
+                (req => req.Method == HttpMethod.Get),
+                ItExpr.IsAny<CancellationToken>()
+                );
+            
+         
+        }
+    }
+}
